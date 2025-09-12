e43b971423c62093aee317717294da3cd3cbee09
Indra-Haha <124240180@student.upnyk.ac.id>
2025-09-12 11:22:10 +0700

    edit-playButton(done)

--- app/components/playButton.tsx
+++ app/components/playButton.tsx
@@ -5,10 +5,10 @@ let showTriangle: string;
 export default function PlayButton() {
     const [isHovered, setIsHovered] = useState<boolean>(false);
     const [showCircle, showTriangle] = isHovered
-        ? ["stroke-red-500 stroke-2", "fill-red-500"]
-        : ["stroke-blue-500 stroke-2", "fill-blue-500"];
+        ? ["stroke-yellow-400 stroke-2", "fill-yellow-400"]
+        : ["stroke-white stroke-2", "fill-white"];
     return (
-        <button onMouseEnter={() => { setIsHovered(true); }} onMouseLeave={() => { setIsHovered(false); }} className="" style={{border: '2px solid black'}}>
+        <button onMouseEnter={() => { setIsHovered(true); }} onMouseLeave={() => { setIsHovered(false); }} className="rounded-2xl" style={{border: '2px solid black'}}>
             <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <circle cx="16" cy="16" r="15.75" className={showCircle} />
                 <path d="M13.25 20.7627L13.25 11.2373L21.5 16L13.25 20.7627Z" className={showTriangle} />
